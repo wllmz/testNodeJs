be050a153f3edb9f93f86836cbe937e455784c4c
unknown <wiwimarti0693@gmail.com>
2023-09-19 13:42:15 +0200

    update requete

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.send('Bonjour, Monde !');
 });
 
-app.listen(port, () => {
-  console.log(`L'application tourne sur http://localhost:${port}`);
+const server = app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
+
+  sendRequests(10).then(() => {
+    server.close();
+  });
 });
+
+const axios = require('axios');
+
+const sendRequests = async (numRequests) => {
+  try {
+    for (let i = 0; i < numRequests; i++) {
+      await axios.get(`http://localhost:${port}/`)
+        .then(response => {
+          console.log(`Requête ${i + 1}:`, response.data);
+        })
+        .catch(error => {
+          console.error(`Erreur sur la requête ${i + 1}:`, error.message);
+        });
+    }
+  } catch (error) {
+    console.error('Erreur globale:', error.message);
+  }
+};
